e9f069b198e10d29de600e69e68e2051de16bc4b
bagbyj <bagbyj@wesclin.org>
2021-06-09 13:46:35 -0500

    Done to the specs of the assignment.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,20 +1,40 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotEmpty(message = "Please enter a username")
+    @Size(min = 5, max = 15)
     private String username;
+    @Email (message = "The email entered does not seem to fit the name@email.com format.")
     private String email;
+    @NotEmpty(message = "Please enter a password")
+    @Size(min = 6)
     private String password;
+
+//    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+//        this.verify = verify;
     }
+
+//    public String getVerify() {
+//        return verify;
+//    }
+//
+//    public void setVerify(String verify) {
+//        this.verify = verify;
+//    }
 
     public String getUsername() {
         return username;
