77acfdb1a0eb57cf67f0f25e631f4fc8abada6ae
bobagby78 <bobagby78@gmail.com>
2021-06-09 12:04:54 -0500

    chap 12 bonus missions on hold. moving on to chap 13 studio.

--- src/main/java/org/launchcode/spaday/UserData.java
+++
@@ -1,8 +0,0 @@
-//package org.launchcode.spaday;
-//
-//import java.util.HashMap;
-//
-//public class UserData {
-//
-//    private static final HashMap<>
-//}
---
+++ src/main/java/org/launchcode/spaday/data/UserData.java
@@ -0,0 +1,20 @@
+//package org.launchcode.spaday.data;
+//
+//import org.launchcode.spaday.models.User;
+//
+//import java.util.Collection;
+//import java.util.HashMap;
+//
+//public class UserData {
+//
+//    public class ClientData {
+//
+//        private static final HashMap<String, User> clients = new HashMap<>();
+//
+//        public static Collection<User> getAll(){
+//            return
+//        }
+//
+//    }
+//
+//}
